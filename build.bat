46968b128af1d1d7e44f25fc9584c9512baf9fb2
descawed <descawed@users.noreply.github.com>
2024-09-29 22:39:40 -0400

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,11 @@
 @echo off
 setlocal
 
-rem TODO: test this
-
 if exist Pmw.py (
     del Pmw.py
 )
 
-for /f "delims=" %%i in ('poetry run python -c "import site; print(site.getsitepackages()[0])"') do set "site_packages=%%i"
+for /f "delims=" %%i in ('poetry run python -c "import site; print(site.getsitepackages()[1])"') do set "site_packages=%%i"
 set "pmw_dir=%site_packages%\Pmw\Pmw_2_1_1"
 rem https://pmw.sourceforge.net/doc/dynamicloader.html
 poetry run python "%pmw_dir%\bin\bundlepmw.py" "%pmw_dir%\lib"
